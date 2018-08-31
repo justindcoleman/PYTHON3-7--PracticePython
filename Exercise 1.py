3ec5027f94b4fe9d9a137b24ab8c0bb75467f40d
Justin Coleman <justindcoleman@users.noreply.github.com>
2018-08-31 16:15:40 -0400

    fixed checks for ints and cast them properly.  added functionality to figure out when the person turns 100.

--- Exercise 1.py
+++ Exercise 1.py
@@ -1,19 +1,36 @@
-correctTypes = False
+correctTypesFirst = False
+correctTypesSecond = False
 name = raw_input('Please enter your name: ')
-currentYear = raw_input('Please enter the current year: ')
 errorMessage = 'This is not a number!'
-if correctTypes == False:
+while correctTypesFirst == False:
     age = raw_input('Please enter your age: ')
     try:
         int(age)
+        correctTypesFirst = True
+
     except ValueError:
         try:
             float(age)
+            correctTypesFirst = True
         except ValueError:
             print errorMessage
 
-print "Hello " + name +"!"
-print "You're currently "+ age + "."
-something = 100 - age
-yearTheyTurnOneHundred = age
+while correctTypesSecond == False:
+    currentYear = raw_input('Please enter the current year: ')
+    try:
+        int(currentYear)
+        correctTypesSecond = True
+
+    except ValueError:
+        try:
+            float(currentYear)
+            correctTypesSecond = True
+        except ValueError:
+            print errorMessage
+
+print "Hello " + name
+print "You're currently "+ age
+differenceBetween = 100 - int(age)
+yearTheyTurnOneHundred = int(currentYear) + int(differenceBetween)
+print "You'll turn 100 in the year: " + str(yearTheyTurnOneHundred)
 raw_input('Press any key to exit')
