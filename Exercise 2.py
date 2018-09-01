a751c8a55b225b9a487e192951e3fcc6f70012f5
Justin Coleman <justindcoleman@users.noreply.github.com>
2018-08-31 22:02:05 -0400

    added second feature dividing to arbitrary numbers

--- Exercise 2.py
+++ Exercise 2.py
@@ -23,3 +23,27 @@ elif divideAnswer is not 0:
     print ('your number is odd!')
 else:
     print('How did you get this error?\n\n \(odd or even determination\)')
+#
+# second part
+#
+isInt = False
+while isInt is False:
+    num = raw_input('Please enter a number: ')
+    check = raw_input('Please enter another number: ')
+    try:
+        int(num)
+        int(check)
+        isInt = True
+
+    except ValueError:
+        try:
+            float(num)
+            float(check)
+            isInt = True
+        except ValueError:
+            print errorMessage
+
+if int(num) % int(check) is 0:
+    print(num + ' divided by ' + check + ' equals zero')
+else:
+    print('those two numbers divided together don\'t equal zero')
