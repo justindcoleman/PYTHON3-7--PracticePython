e57d211e34633ede9915bb461bb17de424fe433b
Justin Coleman <justindcoleman@users.noreply.github.com>
2018-08-31 21:27:46 -0400

    basically functionality working

--- Exercise 2.py
+++ Exercise 2.py
@@ -1,1 +1,23 @@
-userAnswer = raw_input('Please enter a number')
+isInt = False
+errorMessage = 'This is not a number!'
+while isInt is False:
+    userAnswer = raw_input('Please enter a number: ')
+    try:
+        int(userAnswer)
+        isInt = True
+
+    except ValueError:
+        try:
+            float(userAnswer)
+            isInt = True
+        except ValueError:
+            print errorMessage
+
+divideAnswer = int(userAnswer) / 2
+
+if divideAnswer is 0:
+    print ('Your number is even!')
+elif divideAnswer is not 0:
+    print ('your number is odd!')
+else:
+    print('How did you get this error?\n\n \(odd or even determination\)')
