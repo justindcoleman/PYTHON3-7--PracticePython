77fccb45e38f5b7d43da6e98bd7a1dfb77fcb756
Justin Coleman <justindcoleman@users.noreply.github.com>
2018-09-02 01:13:30 -0400

    i was making this way way too complicated when slice reversal made more sense and was easier to both read and write.

--- Exercise 6.py
+++ Exercise 6.py
@@ -1,20 +1,8 @@
-completionState = False
-userList = []
-palindromeCheck = []
-while completionState is not True:
-    userAnswer = raw_input('Please enter a word: ')
-    if userAnswer.isalpha():
-        list(userAnswer)
-        print 'using range:'
-        print userAnswer[0:len(userAnswer)]
-        print 'using only list name:'
-        print userAnswer
-        print 'backwards \(i think\):'
-        print userAnswer[len(userAnswer):0]
-        # lengthOfAnswer = len(userAnswer)
-        # print('okay it\'s working so far')
-        # for i in userAnswer:  # type: object
-        #     print userList
-        #     userList.append(int(userAnswer[i]))
-    else:
-        print ('Please enter a word next time!')
+word = raw_input("Please enter a word:\n")
+word = str(word)
+reverse = word[::-1]
+#print(reverse)
+if word == reverse:
+    print("This word is a palindrome")
+else:
+    print("This word is not a palindrome")
