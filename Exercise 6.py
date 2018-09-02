8788fa92702f5bd3a1069d5d2f02cec19b485834
Justin Coleman <justindcoleman@users.noreply.github.com>
2018-09-02 00:50:45 -0400

    having trouble getting it to reverse but wanted to commit because there's a storm

--- Exercise 6.py
+++ Exercise 6.py
@@ -4,6 +4,17 @@ palindromeCheck = []
 while completionState is not True:
     userAnswer = raw_input('Please enter a word: ')
     if userAnswer.isalpha():
-        print('okay it\'s working so far')
+        list(userAnswer)
+        print 'using range:'
+        print userAnswer[0:len(userAnswer)]
+        print 'using only list name:'
+        print userAnswer
+        print 'backwards \(i think\):'
+        print userAnswer[len(userAnswer):0]
+        # lengthOfAnswer = len(userAnswer)
+        # print('okay it\'s working so far')
+        # for i in userAnswer:  # type: object
+        #     print userList
+        #     userList.append(int(userAnswer[i]))
     else:
-        print('Please enter a word next time!')
+        print ('Please enter a word next time!')
