660a04e2e83cdf3a4ffefa18ca262a7cbe1cdca4
Justin Coleman <justindcoleman@users.noreply.github.com>
2018-12-17 18:20:49 -0500

    done

--- file_operations_advanced_challenge.py
+++ file_operations_advanced_challenge.py
@@ -1,16 +1,16 @@
 #find number of moves to balance packets
-def findMoves(packets): 
+def findMoves(packets):
     sum = 0
     for packet in packets:
-        sum = sum + packet 
+        sum = sum + packet
     if sum % len(packets) is not 0:
         return -1
-  
+
     difference = 0
-    balance = sum / len(packets) 
+    balance = sum / len(packets)
     for packet in packets:
-        difference = difference + abs(packet - balance) 
-    return int(difference / 2) 
+        difference = difference + abs(packet - balance)
+    return int(difference / 2)
 #
 
 #get file and create handle
@@ -21,17 +21,17 @@ packetCount = -1
 packets = []
 end = False
 while end is False:
-  inputs = fileHandle.readline().rsplit() #pull in the number of packets
-  inputs = list(map(int, inputs)) #convert elements to int
-  for input in inputs:
-    if len(packets) < packetCount:  #fills up the list
-      packets.append(input)
-    else:
-      if packetCount is not -1: #runs the main logic
-        print(findMoves(packets))
-        packets = []
-      if input is -1: #final pass
-        end = True
-        break
-      else:
-        packetCount = input #for determining length of the set (list)
+    inputs = fileHandle.readline().rsplit() #pull in the number of packets
+    inputs = list(map(int, inputs)) #convert elements to int
+    for input in inputs:
+        if len(packets) < packetCount:  #fills up the list
+            packets.append(input)
+        else:
+            if packetCount is not -1: #runs the main logic
+                print(findMoves(packets))
+                packets = []
+        if input is -1: #final pass
+            end = True
+            break
+        else:
+            packetCount = input #for determining length of the set (list)
