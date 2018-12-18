c0b034dc6e72b7281be85621bd7fef0c949bc514
Justin Coleman <justindcoleman@users.noreply.github.com>
2018-12-17 21:23:01 -0500

    updated with some basic error handling

--- regex_and_file_input.py
+++ regex_and_file_input.py
@@ -1,14 +1,30 @@
 import re
-fileName = input("Enter a file name (including extension): ")
+#
+while True:
+    fileName = input("Enter a file name (including extension): ")
+    try:
+        fileHandle = open(fileName, 'r')
+        break
+    except FileNotFoundError:
+        (print("{entered} not found, please try again.".format(entered=fileName)))
+#
+#
 userRegex = input("Enter regex expression: ")
-regexMatches = []
-fileHandle = open(fileName, 'r')
+while not userRegex:
+        userRegex = input("Please try again.  Enter regex expression: ")
+#
 
+#
+regexMatches = []
 for line in fileHandle:
     regexMatches.extend(re.findall(userRegex, line))
 
+#
+
+#
 print("Matches:")
 count = 0
 for item in regexMatches:
     count += 1
-    print("Item" + str(count) + ": " + item)
+    print("Found Item " + str(count) + ": " + item)
+#
