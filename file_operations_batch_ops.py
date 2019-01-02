56a387f3f8e7e4fea6d6960704b0e06e4f3eb435
Justin Coleman <justindcoleman@users.noreply.github.com>
2019-01-01 23:39:29 -0500

    basic idea put down as comments

---
+++ file_operations_batch_ops.py
@@ -0,0 +1,10 @@
+# get file location (presumable the native file navigation window can be used?)
+# #try/except for bad input
+# make a list of all the sub directory names (maybe a dict would be better?)
+# crawl the list removing all non-alphanumeric characters (LEAVE ', &, and . in file names tho)
+# rename files to the updated name (some logic for multiple files e.g. file1.txt, file2.txt)
+# pull files into parent directory
+# check that all files have been moved
+# #output log showing any non-empty folders
+# delete empty folders?
+
