e64007736eb8475afd9b709403f03500aa08ebb1
Justin Coleman <justindcoleman@users.noreply.github.com>
2019-01-04 18:58:04 -0500

    basic idea put down as comments, with some imports that i think i'll need

--- file_operations_batch_ops.py
+++ file_operations_batch_ops.py
@@ -1,6 +1,6 @@
-# get file location (presumable the native file navigation window can be used?)
+# get file location (presumable the native file navigation window can be used?) **
 # #try/except for bad input
-# make a list of all the sub directory names (maybe a dict would be better?)
+# make a list of all the sub directory names (maybe a dict would be better?) **
 # crawl the list removing all non-alphanumeric characters (LEAVE ', &, and . in file names tho)
 # rename files to the updated name (some logic for multiple files e.g. file1.txt, file2.txt)
 # pull files into parent directory
@@ -8,7 +8,40 @@
 # #output log showing any non-empty folders
 # delete empty folders?
 
-from os import path #i think this is how you use the native file navigator
-from os import rename #renaming function that can probably be used
-from os import listdir #everything in the directory (files and sub-directories)
-from os import walk #two lists for each directory it visits - splitting into files and directories
+from os import walk  # everything in the directory (files and sub-directories)
+from tkinter.filedialog import askdirectory
+import re
+
+rootList = []
+directoryList = []
+filesList = []
+
+directoriesToWorkOn = askdirectory()
+# directoriesToWorkOn = input("Please enter a directory: ")
+
+# for root, dirs, files in walk(directoriesToWorkOn):
+#     print("root: ", root)
+#     print("directories: ", dirs)
+#     print("files: ", files)
+
+for root, directory, files in walk(directoriesToWorkOn):
+    if root not in rootList:
+        rootList.extend(root)
+    if directory not in directoryList:
+        directoryList.extend(directory)
+    if files not in filesList:
+        filesList.extend(files)
+
+for files in filesList:
+    if [^a-zA-Z0-9.] in files:
+
+
+
+rootLocation = ''.join(rootList[0:3])
+for entry in directoryList:
+    print(entry)
+print()
+print()
+
+
+# print(rootLocation + directoryList[1])
