df48e91cf3a913835ff2053f08a635125a0ac241
justindcoleman <ohmightybiwako@gmail.com>
2019-04-04 09:51:01 -0400

    updated to work in python3.7

--- Exercise 1.py
+++ Exercise 1.py
@@ -1,8 +1,8 @@
-name = raw_input('Please enter your name: ')
+name = input('Please enter your name: ')
 correctTypes = False
 errorMessage = 'This is not a number!'
 while correctTypes is False:
-    age = raw_input('Please enter your age: ')
+    age = input('Please enter your age: ')
     try:
         int(age)
         correctTypes = True
@@ -12,11 +12,11 @@ while correctTypes is False:
             float(age)
             correctTypes = True
         except ValueError:
-            print errorMessage
+            print(errorMessage)
 
 correctTypes = False
 while correctTypes is False:
-    currentYear = raw_input('Please enter the current year: ')
+    currentYear = input('Please enter the current year: ')
     try:
         int(currentYear)
         correctTypes = True
@@ -26,11 +26,11 @@ while correctTypes is False:
             float(currentYear)
             correctTypes = True
         except ValueError:
-            print errorMessage
+            print(errorMessage)
 
 correctTypes = False
 while correctTypes is False:
-    printIterations = raw_input('Enter a number: ')
+    printIterations = input('Enter a number: ')
     try:
         int(printIterations)
         correctTypes = True
@@ -40,15 +40,15 @@ while correctTypes is False:
             float(printIterations)
             correctTypes = True
         except ValueError:
-            print errorMessage
+            print(errorMessage)
 
-print "Hello " + name
-print "You're currently " + age
+print("Hello " + name)
+print("You're currently " + age)
 differenceBetween = 100 - int(age)
 yearTheyTurnOneHundred = int(currentYear) + int(differenceBetween)
 
 while printIterations is not 0:
-    print "You'll turn 100 in the year: " + str(yearTheyTurnOneHundred)
+    print("You'll turn 100 in the year: " + str(yearTheyTurnOneHundred))
     printIterations = int(printIterations) - 1
 
-raw_input('Press any key to exit')
+input('Press any key to exit')
